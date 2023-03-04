44b37caf9e1cef8e2422fa2b3f27a454b4ec0b0e
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-03-04 09:36:40 -0300

    Add files via upload

---
+++ Ordena3Nros.java
@@ -0,0 +1,34 @@
+package casosComIF_ELSE;
+
+import java.util.Scanner;
+
+public class Ordena3Nros {
+
+	public static void main(String[] args) {
+		int nro1, nro2, nro3;
+		int menor, maior, meio;
+		
+		Scanner scanner = new Scanner(System.in);
+
+        // lê os três valores inteiros
+        System.out.println("Digite três valores inteiros (sem repetição):");
+        nro1 = scanner.nextInt();
+        nro2 = scanner.nextInt();
+        nro3 = scanner.nextInt();
+
+        // verifica se não há valores iguais
+        if (nro1 == nro2 || nro1 == nro3 || nro2 == nro3) {
+            System.out.println("Os valores devem ser diferentes!");
+        }
+
+        // organiza os números em ordem crescente
+        menor = Math.min(nro1, Math.min(nro2, nro3));
+        maior = Math.max(nro1, Math.max(nro2, nro3));
+        meio = nro1 + nro2 + nro3 - menor - maior;
+
+        // exibe os números em ordem crescente
+        System.out.printf("Os números em ordem crescente são: %d, %d, %d%n", menor, meio, maior);
+        
+        scanner.close();
+    }
+}
