6615c433609d3a0f78a81ede7c45eecb6740ab64
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-03-04 17:14:26 -0300

    Update VerificaSePrimo.java

--- VerificaSePrimo.java
+++ VerificaSePrimo.java
@@ -4,22 +4,21 @@ import java.util.Scanner;
 
 public class VerificaSePrimo {
 
-	public static void main(String[] args) {
-		int numero, restoDivisao, resultadoDiv, i = 2;;
-		boolean primo = true;
-		Scanner lerDados = new Scanner(System.in);
+     public static void main(String[] args) {
+	int numero, restoDivisao, resultadoDiv, i = 2;;
+	boolean primo = true;
+	Scanner lerDados = new Scanner(System.in);
 
         // lê o número
         System.out.println("Digite um número inteiro:");
         numero = lerDados.nextInt();
         
-        
         do{
         	restoDivisao = numero % i;
         	if (restoDivisao == 0) {
-                primo = false;
-                break;
-            }
+                     primo = false;
+                     break;
+                }
         	resultadoDiv = numero / 2;
         	i++;
         }while(i <= resultadoDiv);
@@ -31,6 +30,6 @@ public class VerificaSePrimo {
             System.out.printf("%d não é um número primo.\n", numero);
         }
 
-	}
+     }
 
 }
